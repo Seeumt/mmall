0c4d6c66f749c3ad83921fae45d9227baa64f43d
Seeumt <Seeumt@cumt.edu.cn>
2019-09-01 14:02:21 +0800

    1400

---
+++ src/main/java/com/mmall/vo/ProductDetailVo.java
@@ -0,0 +1,35 @@
+package com.mmall.vo;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class ProductDetailVo {
+    private Integer id;
+
+    private Integer categoryId;
+
+    private String name;
+
+    private String subtitle;
+
+    private String mainImage;
+
+    private String subImages;
+
+    private String detail;
+
+    private BigDecimal price;
+
+    private Integer stock;
+
+    private Integer status;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private String imgHost;
+
+    private Integer parentCategoryId;
+
+}
